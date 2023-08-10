dec185a499bb2ff20381fccd0bead4642802cde3
Kemelite1 <osikemelite42@gmail.com>
2023-08-10 10:56:36 -0700

    State class inheriting from base class

--- models/state.py
+++ models/state.py
@@ -3,5 +3,9 @@
 from models.base_model import BaseModel
 
 class State(BaseModel):
-    """This is a State class that inherits from BaseModel"""
+    """This is a State class that inherits from BaseModel.
+
+    Attribute:
+        name(str): The name of the state
+    """
     name = ""
