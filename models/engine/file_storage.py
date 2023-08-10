88126be72ebdaea2eaa4b2f20adcf1c429c2f8a1
kumdan job <redeks123456@gmail.com>
2023-08-10 18:49:53 +0100

    unit tessting: completed testing for file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,41 +7,40 @@ import os
 
 
 class FileStorage:
-        """CLASS"""
-        __file_path = 'file.json'
-        __objects = {}
+    """CLASS"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """return the file path"""
+        return self.__objects
+
+    def new(self, obj):
+        """add a new object to the __object attribute"""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
         
-        def all(self):
-                """return the file path"""
-                return self.__objects
-        
-        def new(self, obj):
-                """add a new object to the __object attribute"""
-                self.__objects["{}.{}".format(type(obj).__name__,obj.id)] = obj
-                
-        def save(self):
-                """save the objects to a json file"""
-                with open(self.__file_path , "w") as f:
-                        stored = {key: val.to_dict() for key, val in self.__objects.items()}
-                        json.dump(stored, f ,indent=4)
-        
-        def reload(self):
-                """deserilizes the object from a file"""
-                if os.path.exists(self.__file_path):
-                        with open(self.__file_path, 'r') as f:
-                                obj = json.load(f)
-                                obj = {key:self.classes(val['__class__'])(**val) for key, val in obj.items()}
-                                self.__objects = obj
-                else:
-                        return
-                
-        def classes(self, class_):
-                """returns the class so it can be called somwhere else"""
-                from models.base_model import BaseModel
-                from models.user import User
-                
-                cl = {'BaseModel': BaseModel,
-                      'User': User}
-                if class_ in cl:
-                        return cl[class_]
-                
+    def save(self):
+        """save the objects to a json file"""
+        with open(self.__file_path , "w") as f:
+            stored = {key: val.to_dict() for key, val in self.__objects.items()}
+            json.dump(stored, f ,indent=4)
+
+    def reload(self):
+        """deserilizes the object from a file"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                obj = json.load(f)
+                obj = {key:self.classes(val['__class__'])(**val) for key, val in obj.items()}
+                self.__objects = obj
+        else:
+            return
+
+    def classes(self, class_):
+        """returns the class so it can be called somwhere else"""
+        from models.base_model import BaseModel
+        from models.user import User
+        cl = {'BaseModel': BaseModel, 
+              'User': User}
+        if class_ in cl:
+            return cl[class_]
+            
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,87 +1,57 @@
-# #!/usr/bin/python3
-# """unittest for the FileStorage class"""
+#!/usr/bin/python3
+"""unittest for the FileStorage class"""
 
-# import unittest
-# import models
-# import os
-# import json
-# from datetime import datetime
-# from models.engine.file_storage import FileStorage
-# from models.base_model import BaseModel
-# from models import storage
+import unittest
+import os
+import json
+from datetime import datetime
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import pycodestyle
 
+class TestFileStorage(unittest.TestCase):
+    """"test cases for the FileStorage class"""
 
-# class TestFileStorage(unittest.TestCase):
-#     """"test cases for the FileStorage class"""
+    def test_pepcodestyle(self):
+        """test pepcodestyle"""
+        for path in ['models/base_model.py',
+                     'tests/test_models/test_base_model.py']:
+            with self.subTest(path=path):
+                errors = pycodestyle.Checker(path).check_all()
+                self.assertEqual(errors, 0)
 
-#     def setUp(self):
-#         """sets up test methods"""
-#         pass
+    def test_all(self):
+        """test all method"""
+        stored = FileStorage()
+        self.assertIsInstance(stored.all(), dict)
+        
 
-#     def tearDown(self):
-#         """tear down test methods"""
-#         self.reset_storage()
+    def test_init(self):
+        """Test instantiation of the FileStorage class"""
+        storage = FileStorage()
+        self.assertEqual(type(storage).__name__, "FileStorage")
 
-#     def reset_storage(self):
-#         """Reset FileStorage data"""
-#         FileStorage._FileStorage__objects = {}
-#         if os.path.isfile(FileStorage__file_path):
-#             os.remove(FileStorage.FileStorage__file_path)
+    def test_new(self):
+        """testing the new() method of FileStorage"""
+        stored = FileStorage()
+        bm = BaseModel()
+        stored.new(bm)
+        st = list(stored.all().values())
+        for l in st:
+            if l.id == bm.id:
+                self.assertEqual(l, bm)
 
-#     def test_file_storage_instantiation(self):
-#         """Test instantiation of the FileStorage class"""
-#         storage = FileStorage()
-#         self.assertEqual(type(storage).__name__, "FileStorage")
-
-#     def test_new_method(self):
-#         """testing the new() method of FileStorage"""
-#         storage = FileStorage()
-#         instance = FileStorage()
-#         instance.id = "test_id"
-#         instance.key = "value"
-#         storage.new(instance)
-#         self.assertTrue("FileStorage.test_id" in storage.all())
-#         self.assertEqual(storage.all()["FileStorage.test_id"], instance)
-
-#     def test_save_method(self):
-#         """testing the save() method of FileStorage"""
-#         storage = FileStorage()
-#         instance = FileStorage()
-#         instance.id = "test_id"
-#         instance.key = "value"
-#         storage.new(instance)
-
-#         with open('file.json', 'w') as f:
-#             f.write("") #empty file for testing
-
-#         storage.save()
-
-#         with open('file.json', 'r') as f:
-#             data = json.load(f)
-#             self.assertIn("FileStorage.test_id", data)
-#             self.assertEqual(data["FileStorage.test_id"]["id"], "test_id")
-#             self.assertEqual(data["FileStorage.test_id"]["key"], "value")
-
-#     def test_reload_method(self):
-#         """"testing the reload() method of FileStorage"""
-#         instance_data = '{"FileStorage.test_id": {"id": "test_id", "key": "value"}}'
-#         with open('file.json', 'w') as f:
-#             f.write(instance_data)  # Write instance data to the file
-
-#         storage = FileStorage()
-#         storage.reload()
-#         self.assertIn("FileStorage.test_id", storage.all())
-#         instance = storage.all()["FileStorage.test_id"]
-#         self.assertEqual(instance.__class__.__name__, "FileStorage")
-#         self.assertEqual(instance.id, "test_id")
-#         self.assertEqual(instance.key, "value")
-
-#     def test_classes_method(self):
-#         """testing the classes() method of FileStoraage"""
-#         storage = FileStorage()
-#         classes_method = storage.classes()
-#         self.assertEqual(classes_method.__name__, "BaseModel")
+    def test_save_and_reload(self):
+        """testing the save() method of FileStorage"""
+        stored = FileStorage()
+        bm = BaseModel()
+        stored.new(bm)
+        stored.save()
+        stored.reload()
+        
+        st = stored.all()
+        self.assertIn("{}".format(type(bm).__name__ + '.'+ bm.id), st)
 
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
