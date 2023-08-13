f1f141191c6e786338f21c477bf0f94f99b0df0b
kumdan job <redeks123456@gmail.com>
2023-08-14 00:16:43 +0100

    unittest console.py: trial

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python3
-"""unittest for the console.py"""
+"""
+Contains the class TestConsoleDocs
+"""
+
+import console
+import inspect
 import unittest
-import sys
-import os
-from io import StringIO
-from unittest.mock import patch
-from models import storage
-from models.engine.file_storage import FileStorage
-from console import HBNBCommand
+HBNBCommand = console.HBNBCommand
 
-class TestHBNBCommand(unittest.TestCase):
-    """unittest for the HBNB command line interpreter"""
-    console = HBNBCommand()
 
-    def test_quit(self):
-        """Testing the quit command"""
-        with patch('builtins.input', return_value="quit"):
-            self.assertTrue(self.console.onecmd("quit"))
+class TestConsoleDocs(unittest.TestCase):
 
-    def test_EOF(self):
-        """Testing the EndOfFile command"""
-        with patch('builtins.input', return_value="EOF"):
-            self.assertTrue(self.console.onecmd("EOF"))
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
